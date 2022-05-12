d01c433d2fe8d14a7f280e4bb7df267ba78569a9
DimitrisGrigoropoulos <79205642+DimitrisGrigoropoulosP2016099@users.noreply.github.com>
2022-05-12 21:58:11 +0300

    Add files via upload

--- Ergasia.m
+++ Ergasia.m
@@ -5,15 +5,16 @@ pkg load signal;
 
 i = argv();
 for a = 1:nargin
-filename = i{1,a};
-[y,fs]=audioread(i{1,a});
+filename = i{a,1};
+[y,fs]=audioread(i{a,1});
 
 
-b = fir1(500,0.2,"low");
+b = fir1(10,0.2,"low");
 
 ff = filter(b,1,y);
 
-b= fir1(501,0.10,"high");
+b= fir1(74,0.10,"high");
 ff=filter(b,1,ff);
-audiowrite('t1_',filename,ff*3,fs);
+name = strjoin({'neo',filename}, '_')
+audiowrite(name,ff*3,fs);
 endfor
