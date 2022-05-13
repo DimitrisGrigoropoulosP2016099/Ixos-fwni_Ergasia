0226e3b8a7f55b66bc8113f0f5d2fbf4b40a2c25
DimitrisGrigoropoulos <79205642+DimitrisGrigoropoulosP2016099@users.noreply.github.com>
2022-05-13 18:23:56 +0300

    Update Ergasia.m

--- Ergasia.m
+++ Ergasia.m
@@ -3,18 +3,25 @@ close all;
 clf;
 pkg load signal;
 
+# to argv() eina eidiko function tou octave gia na pernas arguments
 i = argv();
+# epanalipsi apo 1 mexri ton arithmo twn arguments pou dothikan
 for a = 1:nargin
 filename = i{a,1};
+# diabazei ta dedomena tou arxeiou kai to sampling rate tou 
 [y,fs]=audioread(i{a,1});
 
+# pername prwta to low-pass filtro
+file = fir1(10,0.2,"low");
 
-b = fir1(10,0.2,"low");
+finalfile = filter(file,1,y);
 
-ff = filter(b,1,y);
-
-b= fir1(74,0.10,"high");
-ff=filter(b,1,ff);
+# kai edw to high-pass
+file= fir1(74,0.10,"high");
+# ta enwnoume
+finalfile=filter(file,1,finalfile);
+# dimiougoume ena neo onoma gia to arxeio
 name = strjoin({'neo',filename}, '_')
-audiowrite(name,ff*3,fs);
+# kai to apothikeuoume 
+audiowrite(name,finalfile*3,fs);
 endfor
